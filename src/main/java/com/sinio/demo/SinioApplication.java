1143912451fe8817beeb0fc0c41966570752a802
Renza Alvianino <email_renza@gmail.com>
2025-10-29 20:01:24 +0700

    Initial commit - Spring Boot project

---
+++ src/main/java/com/sinio/demo/SinioApplication.java
@@ -0,0 +1,13 @@
+package com.sinio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinio/demo/SinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinio.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
